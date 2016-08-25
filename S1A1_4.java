3e08276e0e506abe84a7766c6d2d56fda192e6ed
adityavgp <gpaditya11@gmail.com>
2016-08-25 20:45:21 +0530

    Add files via upload

---
+++ S1A1_4.java
@@ -0,0 +1,12 @@
+
+public class S1A1_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 65; i <= 90; i++) {
+			System.out.println("Char for ASCII value of "+i+" is "+(char)i);
+		}
+
+	}
+
+}
